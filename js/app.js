475fc478f05be6f38c5c29814e16f10f8d2ae57b
aaronmclean1 <aaronmclean@sbcglobal.net>
2014-12-10 21:51:23 -0600

    added comments

--- js/app.js
+++ js/app.js
@@ -1,197 +1,160 @@
 var pickPlayer = true;
-var currentChar = 1;
 
-var charImages = 
+var characterPngs = 
 [
 'images/char-boy.png',
 'images/char-cat-girl.png',
 'images/char-horn-girl.png',
 'images/char-pink-girl.png',
 'images/char-princess-girl.png'
-];    
-    
+];	
+	
 
 // Enemies our player must avoid
 var Enemy = function(x, y, speed) 
 {
-	// Variables applied to each of our instances go here,
-	// we've provided one for you to get started
-
-	// The image/sprite for our enemies, this uses
-	// a helper we've provided to easily load images
 	this.sprite = 'images/enemy-bug.png';
 	this.speed = speed;
-
 	this.x = x;
 	this.y = y;   
 	
 }
 
-var Initialize = function() 
+// Update the enemy's position
+Enemy.prototype.update = function(dt) 
 {
-	allEnemies = [];
+	//moving the enemy by speed and level of the game. The higher the level the faster the enemies move, the harder the game.
+	this.x += this.speed * dt * player.level;
 	
-	this.player = new Player(0,0);	
-	
-	//this.level = $('#level').val();
-		
-	for (var i = 1; i < 4; ++i) 
-	{	
-		var enemy = new Enemy(1,i,i);
-		allEnemies.push(enemy);	
-	}    
+	if( this.x > 6)
+	{
+		this.x = -1;		
+	}   
 }
 
-
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
-Enemy.prototype.update = function(dt, level) 
-{
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
-    this.x += this.speed * dt * level;
-    
-    if( this.x > 6)
-    {
-    	this.x = -1;    	
-    }   
-}
-
-// Draw the enemy on the screen, required method for game
+// Draw the enemy on the screen
 Enemy.prototype.render = function()
-{       
-    ctx.drawImage(Resources.get(this.sprite), this.x * 101, this.y * 83 - 20);    
+{	   
+	ctx.drawImage(Resources.get(this.sprite), this.x * 101, this.y * 83 - 20);	
 }
 
-// Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
-
-
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
-
-
+//Set default params for a player
 var Player = function(x,y) 
-{
-	this.sprite = 'images/char-boy.png';	
+{	
+	this.lives = 3;
+	this.level = 1;
+	this.x = x;
+	this.y = y;   	
 }
 
-Player.prototype.update = function (x,y) 
-{
-
-
-};
-
+//draw the players on the screen
 Player.prototype.render = function(x,y)
-{   
-	var selectorImage = 'images/Selector.png';
- 
+{	
+	//pick a player at the beginning of the game
 	if (pickPlayer == true)
 	{	
+		//print some instructions to the screen for the user
 		ctx.font = "38px Arial";
 		ctx.fillStyle = 'green';	
 		ctx.fillText("Let's Go For A Swim!",70,187);
 		ctx.fillText("Select A Player And Hit Enter",5,267);
 		ctx.fillText("Use The Arrow Keys To Move",0,347);
 		
+		//draw selector png in the middle bottom of screen
 		this.y = 5;
-		ctx.drawImage(Resources.get(selectorImage), this.x * 101, this.y * 83 - 20);
+		ctx.drawImage(Resources.get('images/Selector.png'), this.x * 101, this.y * 83 - 20);
 
-		for (var i = 0; i < charImages.length; i++)
+		//draw characters on screen
+		for (var i = 0; i < characterPngs.length; i++)
 		{
-			ctx.drawImage(Resources.get(charImages[i]), i * 100, this.y * 83 - 20);
-		}		
+			ctx.drawImage(Resources.get(characterPngs[i]), i * 100, this.y * 83 - 20);
+		}
 	} 
+	
+	// if not beginning of game then draw the normal player, lives, and level
 	else
 	{
-		ctx.drawImage(Resources.get(this.sprite), this.x * 101, this.y * 83 - 20);
-		
+		ctx.drawImage(Resources.get(this.sprite), this.x * 101, this.y * 83 - 20);		
 		ctx.font = "18px Arial";
 		ctx.fillStyle = 'black';		
-		ctx.fillText("Level: " + level,10,580);		
-		ctx.fillText("Lives: " + lives,110,580);		
-	}  
-    
+		ctx.fillText("Level: " + player.level,10,580);		
+		ctx.fillText("Lives: " + player.lives,110,580);		
+	}
 }
 
-Player.prototype.reset = function ()
-{    
-    this.x = 201;
-    this.y = 400;
-};
-
+//this code executes based on what key the user pressed
 Player.prototype.handleInput = function (key) 
 {
-    switch (key) 
-    {
-        case 'left':
-            if (this.x > 0) 
-            {
-                this.x--;
-            }
-            break;
-        case 'up':            
-            if (this.y > 0) 
-            {
-                this.y--;
-            }
-            else
-            {
-        		ctx.clearRect(0, 0, 500, 600);            	
-            	level++;
-            	this.y = 5;            	
-            }            
-            break;
-        case 'right':
-            if (this.x < 4) 
-            {
-                this.x++;
-            }
-            break;
-        case 'down':
-            if (this.y < 5) 
-            {
-                this.y++;
-            }
-            break;
-        case 'enter':        	
-            pickPlayer = false;
-            this.y = 5;
-            this.sprite = charImages[this.x];               
-            break;
-        case 'quit':
+	switch (key) 
+	{
+		//if the user pressed the left keyboard button move player left one x value
+		case 'left':
+			if (this.x > 0) 
+			{
+				this.x--;
+			}
+			break;
+		//if the user pressed the up keyboard button move player up one y value		
+		case 'up':			
+			if (this.y > 0) 
+			{
+				this.y--;
+			}
+			//if the player made it to the top of the screen increment the Level value, clear the screen (due to the players big heads), and move player back to bottom of screen
+			else
+			{
+				ctx.clearRect(0, 0, 500, 600);				
+				player.level++;
+				this.y = 5;				
+			}			
+			break;
+		//if the user pressed the right keyboard button move player right one x value
+		case 'right':
+			if (this.x < 4) 
+			{
+				this.x++;
+			}
+			break;
+		//if the user pressed the down keyboard button move player down one y value				
+		case 'down':
+			if (this.y < 5) 
+			{
+				this.y++;
+			}
+			break;
+		//used to select a player
+		case 'enter':			
+			pickPlayer = false;
+			this.y = 5;
+			this.sprite = characterPngs[this.x];			   
+			break;
+		//used to quit and restart game
+		case 'quit':
 			pickPlayer = true;
 			this.player.y = 5;	
-			lives = 3;
-			level = 1;
-            break;            
-        default:
-            break;
-    } 
+			player.lives = 3;
+			player.level = 1;
+			break;			
+		default:
+			break;
+	} 
 
 };
 
-
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
-$(document).keydown
-	(
-		function(e) 
+//listen for these keys to be pressed
+document.addEventListener
+('keydown', function(e) 
+	{
+		var allowedKeys = 
 		{
+					13: 'enter',
+					37: 'left',
+					38: 'up',
+					39: 'right',
+					40: 'down',
+					81: 'quit'
+		};
 
-			var allowedKeys = 
-			{
-				13: 'enter',
-				37: 'left',
-				38: 'up',
-				39: 'right',
-				40: 'down',
-				81: 'quit'
-			};
-
-			player.handleInput(allowedKeys[e.keyCode]);	     
-		}
-	)
+		player.handleInput(allowedKeys[e.keyCode]);
+	}
+);	
